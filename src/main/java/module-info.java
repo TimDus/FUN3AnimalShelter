c9fedee0cab06204ae605526ef1b47a14c33b8ea
TimDus <timvddussen@gmail.com>
2021-01-13 03:17:56 +0100

    Test

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module FUN3AnimalShelter
-{
-    requires javafx.base;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires java.sql;
-    requires javafx.fxml;
-    exports GUI;
-}
