930bab6a7c910443b6c222bd53818e7fd4eb757b
TimDus <timvddussen@gmail.com>
2021-01-13 03:01:29 +0100

    fix gui

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module FUN3AnimalShelter
+{
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.sql;
+    requires javafx.fxml;
+    exports GUI;
+}
